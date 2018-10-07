7ac6477a49c59ae61b98ff5eb5963fe8d5f26649
Burakcan Timuçin <timucinburakcan@gmail.com>
2018-10-07 15:17:21 +0300

    Initial commit

---
+++ generate_data.py
@@ -0,0 +1,30 @@
+import csv
+from decimal import Decimal
+import random
+import datetime
+
+with open("/home/burakcan/Desktop/BitirmeProjesi/temp_data.csv") as myfile:
+    data = csv.reader(myfile, delimiter=',')
+    temps = []
+    for i in data:
+        temps.append(i[5])
+
+temps.pop(0)
+
+minTemp = min(temps)
+minTemp = round(Decimal(minTemp), 0)
+
+maxTemp = max(temps)
+maxTemp = round(Decimal(maxTemp), 0)
+
+digit = random.randint(1,15)
+
+newTemp = round(random.uniform(int(minTemp), int(maxTemp)), digit)
+
+now = datetime.datetime.now()
+
+with open("/home/burakcan/Desktop/BitirmeProjesi/temp_data.csv", "a") as csvfile:
+    fieldnames = ["day", "month", "year", "hour", "minute", "temp"]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writerow({"day": now.day, "month": now.month, "year": now.year,
+                    "hour": now.hour, "minute": now.minute, "temp": newTemp})
